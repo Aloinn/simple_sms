1c71c5112ebbbe0b66a1b3f3d18848ed64f5197d
Alain Nguyen <onealainn@gmail.com>
2020-02-25 15:01:03 -0500

    Changed async to callback

--- Handlers/handleChat.js
+++ Handlers/handleChat.js
@@ -110,19 +110,6 @@ module.exports = function (socket, io) {
     })
     chat_id = chat._id;
     var room = new Room(chat_id);
-    // CONNECT USERS
-    for(let user of user_list){
-      for(id in connections){
-        if(String(connections[id].id)==String(user.model_id)){
-          var _user =  User.findOne({id: ObjectId(user.model_id)})
-                            .populate({path: 'group_chat', populate:{path: 'users', select: 'username', model:'User'}})
-                            .populate({path: 'single_chat.user', select: 'username', model: 'User'})
-                            .exec();
-          sockets[id].emit('update-user', _user);
-          break;
-        }
-      }
-    }
 
     // CONNECT USERS
     async.each(user_list, (user)=>{
@@ -138,6 +125,19 @@ module.exports = function (socket, io) {
        }
      });
 
+     // CONNECT USERS
+     for(let user of user_list){
+       for(id in connections){
+         if(String(connections[id].id)==String(user.model_id)){
+           var _user =  User.findOne({id: ObjectId(user.model_id)})
+                             .populate({path: 'group_chat', populate:{path: 'users', select: 'username', model:'User'}})
+                             .populate({path: 'single_chat.user', select: 'username', model: 'User'})
+                             .exec();
+           sockets[id].emit('update-user', _user);
+           break;
+         }
+       }
+     }
      io.in(room.id).emit('chat-updated', {room:room});
 
   })
