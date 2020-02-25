268c16311086ddb8193ba3c9e6576e9078429b39
Alain Nguyen <onealainn@gmail.com>
2020-02-25 12:14:12 -0500

    TEST

--- Handlers/handleAuth.js
+++ Handlers/handleAuth.js
@@ -3,7 +3,7 @@ const config = process.env;
 const jwt = require('jsonwebtoken');
 const Crypto = require('crypto');
 const path = require('path');
-var User = require('/models/user');
+var User = require('/app/models/user');
 
 // HANDLE AUTHENTICATION CALLS
 module.exports = function (socket) {
