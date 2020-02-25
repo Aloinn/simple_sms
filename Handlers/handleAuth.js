f32aedfd64633388c14861b2e00c86f92e885ce7
Alain Nguyen <onealainn@gmail.com>
2020-02-25 11:21:56 -0500

    Changed dir again

--- Handlers/handleAuth.js
+++ Handlers/handleAuth.js
@@ -2,7 +2,7 @@
 const config = process.env;
 const jwt = require('jsonwebtoken');
 const Crypto = require('crypto');
-const User = require(__dirname +'/models/user');
+const User = require('../models/user');
 
 // HANDLE AUTHENTICATION CALLS
 module.exports = function (socket) {
