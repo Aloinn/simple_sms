95d0a2325f8e33077d79c9bc94f79886496d6e1e
Alain Nguyen <onealainn@gmail.com>
2020-02-25 12:26:44 -0500

    T

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 // db.js
 var mongoose = require('mongoose');
 var config = process.env;
-//mongoose.connect(config.mongoDB, {useFindAndModify: false,useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(config.mongoDB, {useFindAndModify: false,useNewUrlParser: true, useUnifiedTopology:true})
